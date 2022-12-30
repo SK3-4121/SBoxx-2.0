20e290d26c7cd99ac2c1b84296d400649535ccdc
SK3#3160 <66770178+SK3-4121@users.noreply.github.com>
2022-12-30 13:35:59 -0800

    Update libhookz.js

--- models/libhookz.js
+++ models/libhookz.js
@@ -1,11 +1,16 @@
 class libhookz {
-    constructor(link, hex, top, title, description, setting) {
+    constructor(real, link, hex, title, description, setting, author_name, author_url, provider_name, provider_url) {
+        this.real = real;
         this.link = link;
         this.hex = hex;
-        this.top = top;
         this.title = title;
         this.description = description;
         this.setting = setting;
+
+        this.author_name = author_name;
+        this.author_url = author_url;
+        this.provider_name = provider_name;
+        this.provider_url = provider_url;
     }
 
     one() {
@@ -49,7 +54,11 @@ class libhookz {
             <meta name="twitter:player:stream:content_type" content="video/mp4" />
             <meta property="og:image" content="${this.link}"/>
             <meta name="twitter:card" content="summary_large_image">
+            
             <meta name="twitter:title" content="${this.title}" />
+            <meta name="og:title" content="${this.title}" />
+
+            <meta name="og:description" content="${this.description}" />
             <meta name="twitter:description" content="${this.description}" />
         </head>
     </html>`;
@@ -66,9 +75,14 @@ class libhookz {
             <meta name="twitter:player:stream:content_type" content="video/mp4" />
             <meta property="og:image" content="${this.link}"/>
             <meta name="twitter:card" content="summary_large_image">
+
             <meta name="twitter:title" content="${this.title}" />
+            <meta name="og:title" content="${this.title}" />
+
+            <meta name="og:description" content="${this.description}" />
             <meta name="twitter:description" content="${this.description}" />
-            <link type="application/json+oembed" href="${this.top}" />
+
+            <link type="application/json+oembed" href="http://${this.real}/jason?an=${this.author_name}&au=${this.author_url}&pn=${this.provider_name}&pu=${this.provider_url}" />
         </head>
     </html>`;
     }
@@ -84,9 +98,15 @@ class libhookz {
             <meta name="twitter:player:stream:content_type" content="video/mp4" />
             <meta property="og:image" content="${this.link}"/>
             <meta name="twitter:card" content="summary_large_image">
+
             <meta name="twitter:title" content="${this.title}" />
+            <meta name="og:title" content="${this.title}" />
+
             <meta name="twitter:description" content="${this.description}" />
-            <link type="application/json+oembed" href="${this.top}" />
+            <meta name="og:description" content="${this.description}" />
+
+            <link type="application/json+oembed" href="http://${this.real}/jason?an=${this.author_name}&au=${this.author_url}&pn=${this.provider_name}&pu=${this.provider_url}" />
+
             <meta name="theme-color" content="${this.hex}"/>
         </head>
     </html>`;
